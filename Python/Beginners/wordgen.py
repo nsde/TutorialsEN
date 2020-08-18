cd504ec788a05b7389da2548f6037c8c28c5f860
Felix <67185896+nsde@users.noreply.github.com>
2020-08-18 16:59:15 +0200

    Create wordgen.py

---
+++ Python/Beginners/wordgen.py
@@ -0,0 +1,27 @@
+
+# Programmier-Tutorials
+Der Code aller Tutorials auf YouTube und dem Blog!
+
+Bei Vorschlägen, Hilfe oder Sonstigem sehr gerne auf Discord schreiben:
+**https://bit.ly/styscord**. Notfalls geht auch Twitter: **@neostyxde**.
+
+## Eine Bitte!
+Die Tutorials sind für DICH und alle, die auch am Programmieren Interessiert sind! Deswegen bitten wir dich sehr, Vorschläge und Feedback zu schreiben. Danke vielmals!
+
+## Kategorien
+**Selbstverständlich empfiehlt es sich sehr, die Kategorien in derselben Reihenfolge, wie wir sie geschreiben haben, zu begutachten.**
+***
+
+ ### Anfänger
+Alles, was man zum Einsteigen braucht: z.B. **Syntax**, **der Umgang mit Fehlern** und einiges mehr. Fallst du z.B. im Informatikunterricht Probleme hast, kannst du gerne hier reinschauen und stöbern.
+
+ ### Fortgeschrittene
+ Hier werden unter Anderem **Erweiterungen**, **Add-ons**, **Plugins** oder **Module** genauer erläutert. Wenn du nicht nur die Sprache lernen willst, sondern auch interesannte Projekte erstellen möchtest, bist du hier bestens bedient.
+ 
+ ### Projekte
+ Wenn man mit den Kategorien "Anfänger" und "Fortgeschrittene" genug herumgestöbert hat, kann man hier endlich seiner Kreativität freien Lauf lassen! In den Kategorien "Anfänger" und "Fortgeschrittene" werden nur Test-Projekte zum Lernen erstellt, doch hier kann man sein Wissen benutzen, um richtige Ideen zu erschaffen.
+ ***
+ 
+ ## Danke
+ Herzlichen Dank and DICH und alle anderen fürs Lesen! Wir wünschen viel Spaß und Erfolg!
+ *-Team NeoStyx*
